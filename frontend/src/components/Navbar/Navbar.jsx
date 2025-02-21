9a0e165d2ed028a18630345674661dd3c741b0a5
shades-of-prakash <prakashvarma00045@gmail.com>
2025-02-21 09:23:56 +0530

    Added AdminLayout

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import "./Navbar.css"
+import svg from "../../assets/svg.svg"
 const Navbar=()=>{
-    return <div className="Navbar">Navbar</div>
+    return <div className="Navbar">Navbar  <div className="test"> <img src={svg} /></div></div>
 }
 export default Navbar
--- frontend/src/contexts/ThemeContext.jsx
+++ frontend/src/contexts/ThemeContext.jsx
@@ -24,6 +24,7 @@ const ThemeProvider=({children})=>{
 
     useEffect(() => {
         const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+        console.log(mediaQuery)
 
         const handleChange = (e) => {
             if (!localStorage.getItem("theme")) {
