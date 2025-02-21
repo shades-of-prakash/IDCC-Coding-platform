f9d16d11a094f8b67c90d67cbd95c25bb49c04ce
shades-of-prakash <prakashvarma00045@gmail.com>
2025-02-21 23:02:21 +0530

    Added Sidemenu

--- frontend/src/components/Sidemenu/Sidemenu.jsx
+++ frontend/src/components/Sidemenu/Sidemenu.jsx
@@ -1,5 +1,34 @@
+import {Link } from "react-router";
+import logo from "../../assets/logo.png"
+import {Category,DocumentCode,MedalStar,Setting2 }from "iconsax-react"
+import "./Sidemenu.css"
+import UseTheme from "../../hooks/UseTheme"
 const SideMenu=()=>{
-    return <div className="sidemenu">sidebar</div>
+    const {theme}=UseTheme()
+    return <div className="sidemenu df df_ai_center df_fd_c">
+        <div className="logo df df_ai_c">
+            {/* <img className="logo_img" src={logo} alt="Logo of IDCC" /> */}
+            <p>IDCC</p>
+        </div> 
+        <ul className="menu_items df df_fd_c">
+            <li className="menu_item df df_ai_center">
+                <Category color="var(--secondary-color)"/>
+                <Link>Overview</Link>
+            </li>
+            <li className="menu_item df df_ai_center">
+                <MedalStar color="var(--secondary-color)"/>
+                <Link>Contest</Link>
+            </li>
+            <li className="menu_item df df_ai_center">
+                <DocumentCode color="var(--secondary-color)"/>
+                <Link>Submissions</Link>
+            </li>
+            <li className="menu_item df df_ai_center">
+                <Setting2 color="var(--secondary-color)"/>
+                <Link>Settings</Link>
+            </li>
+        </ul>
+    </div>
 }
 
 export default SideMenu
