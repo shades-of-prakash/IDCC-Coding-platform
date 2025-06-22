807d33278a5bf1b015dfc29e47fda0a418ff5f17
shades-of-prakash <prakashvarma00045@gmail.com>
2025-06-22 21:20:09 +0530

    Added Admin login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,24 @@ import { ThemeProvider } from "./contexts/ThemeContext";
 import FileUpload from "./components/Upload";
 import Login from "./pages/Login.jsx";
 import Landing from "./pages/Landing.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import AdminLogin from "./pages/AdminLogin.jsx";
 function App() {
 	return (
 		<ThemeProvider>
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Landing />} />
+					<Route path="/admin-login" element={<AdminLogin />} />
 					<Route path="/login" element={<Login />} />
-					<Route path="/admin" element={<AdminLayout />}>
+					<Route
+						path="/admin"
+						element={
+							<ProtectedRoute>
+								<AdminLayout />
+							</ProtectedRoute>
+						}
+					>
 						<Route index element={<Navigate to="overview" />} />
 						<Route path="overview" element={<div>Overview...</div>} />
 						<Route path="contest" element={<FileUpload />} />
---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+// components/ProtectedRoute.jsx
+import { Navigate } from "react-router";
+
+const ProtectedRoute = ({ children }) => {
+	const token = localStorage.getItem("token");
+
+	if (!token) {
+		return <Navigate to="/admin-login" replace />;
+	}
+
+	return children;
+};
+
+export default ProtectedRoute;
